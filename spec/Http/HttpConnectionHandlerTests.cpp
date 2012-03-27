0232f1459aca0d7aceb31fc999d19acf3baa7fb1
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-27 14:20:22 -0500

    One more test and refactoring.

--- spec/Http/HttpConnectionHandlerTests.cpp
+++ spec/Http/HttpConnectionHandlerTests.cpp
@@ -7,18 +7,26 @@ class HttpConnectionHandlerTester
   : public ::testing::Test
 {
   public:
+      MockSocketReader socketReader_;
+      MockHttpRequestHandlerFactory factory_;
+      HttpConnectionHandler handler_;
+
     HttpConnectionHandlerTester()
+      : socketReader_()
+      , factory_()
+      , handler_( socketReader_, factory_ )
     { }
 };
 
 TEST_F( HttpConnectionHandlerTester, readsToEnd )
 {
-  MockSocketReader socketReader;
-  MockHttpRequestHandlerFactory factory;
+  handler_.handle( 5 );
+  ASSERT_EQ( true, socketReader_.readToEnd_ );
+}
 
-  HttpConnectionHandler handler( socketReader, factory ); 
-  handler.handle( 5 );
-
-  ASSERT_EQ( true, socketReader.readToEnd_ );
+TEST_F( HttpConnectionHandlerTester, readsOnSocketItReceives )
+{
+  handler_.handle( 8 );
+  ASSERT_EQ( 8, socketReader_.socketFDRead_ );
 }
 
