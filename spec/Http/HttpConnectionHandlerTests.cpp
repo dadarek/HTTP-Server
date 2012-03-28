6d8329610ea1845304e25ca9f98b9e1a89db436a
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-28 17:05:52 -0500

    Renaming variable.

--- spec/Http/HttpConnectionHandlerTests.cpp
+++ spec/Http/HttpConnectionHandlerTests.cpp
@@ -15,7 +15,7 @@ class HttpConnectionHandlerTester
       MockHttpRequestParser parser_;
       MockHttpRequestHandler requestHandler_;
       MockHttpRequestHandlerFactory factory_;
-      HttpConnectionHandler handler_;
+      HttpConnectionHandler connectionHandler_;
 
       HttpRequestInspector requestInspector_;
       HttpResponseInspector responseInspector_;
@@ -27,7 +27,7 @@ class HttpConnectionHandlerTester
       , parser_()
       , requestHandler_()
       , factory_()
-      , handler_( socketReader_, parser_, factory_ )
+      , connectionHandler_( socketReader_, parser_, factory_ )
       , requestInspector_()
       , responseInspector_()
       , request_( new MockHttpRequest( requestInspector_ ) )
@@ -41,48 +41,47 @@ class HttpConnectionHandlerTester
 
 TEST_F( HttpConnectionHandlerTester, readsToEnd )
 {
-  handler_.handle( 5 );
+  connectionHandler_.handle( 5 );
   ASSERT_EQ( true, socketReader_.readToEnd_ );
 }
 
 TEST_F( HttpConnectionHandlerTester, readsOnSocketItReceives )
 {
-  handler_.handle( 8 );
+  connectionHandler_.handle( 8 );
   ASSERT_EQ( 8, socketReader_.socketFDRead_ );
 }
 
 TEST_F( HttpConnectionHandlerTester, forwardsSocketDataToParser )
 {
   socketReader_.readToEndReturnValue_ = "Hi there";
-  handler_.handle( 8 );
+  connectionHandler_.handle( 8 );
   ASSERT_STREQ( "Hi there", parser_.stringToParse_.c_str() );
 }
 
 TEST_F( HttpConnectionHandlerTester, forwardsParserDataToFactory )
 {
-  handler_.handle( 8 );
+  connectionHandler_.handle( 8 );
   ASSERT_EQ( request_, factory_.requestReceived_ );
 }
 
 TEST_F( HttpConnectionHandlerTester, forwardsRequestToHandlerFromFactory )
 {
-  handler_.handle( 8 );
+  connectionHandler_.handle( 8 );
   ASSERT_EQ( request_, requestHandler_.requestReceived_ );
 }
 
 TEST_F( HttpConnectionHandlerTester, deletesRequest )
 {
-  handler_.handle( 8 );
+  connectionHandler_.handle( 8 );
   ASSERT_EQ( true, requestInspector_.destroyed );
 }
 
 TEST_F( HttpConnectionHandlerTester, deletesResponses )
 {
-  handler_.handle( 8 );
+  connectionHandler_.handle( 8 );
   ASSERT_EQ( true, responseInspector_.destroyed );
 }
 
-// make sure it deletes the request and response
 // make sure it deletes response after writing
 // make sure it deletes the HttpHandler
 // HttpConnectionHandler takes a SocketReader AND a SocketAPI? I don't like that ...
