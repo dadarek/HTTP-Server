3e65906e9c1d23775bfe6972477e15094287289f
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-28 16:25:30 -0500

    Rigging main just to fake some cobspec tests.

--- lib/Main.cpp
+++ lib/Main.cpp
@@ -17,15 +17,13 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sstream>
 
 int a()
 {
 
-  std::cout << "Hi" << std::endl;
-  std::cout << INADDR_ANY << std::endl;
-
   RawSocketApi socketApi;
-  SocketConnectionReceiver receiver( socketApi, 8083 );
+  SocketConnectionReceiver receiver( socketApi, 5000 );
   HttpSocketReader reader( socketApi );
 
   for(;;)
@@ -41,10 +39,26 @@ int a()
     delete request;
 
 
-    printf("Url: %s\n", url.c_str() );
-    printf("ALL HEADERS\n--------\n%s\n", requestHeaders.c_str() );
+    std::string newLine = "\r\n";
 
-    write(nextConnection, "I got your stuffing...", 22);
+    std::string body = "<html><head><title></title><head><body><a href=\"file1\">file1</a><br><a href=\"file2\">file2</a><br></body></html>";
+    std::stringstream ss;
+    ss << (body.length() + 1);
+
+    std::string bodyLength = ss.str();
+
+    std::string headers;
+    headers += "HTTP/1.1 200 OK" + newLine;
+    headers += "Status: 200 OK" + newLine;
+    headers += "Date: Wed, 28 Mar 2012 11:03:32 GMT" + newLine;
+    headers += "Connection: close" + newLine;
+    headers += "Server: cplus/0.3" + newLine;
+    headers += "Content-Type: text/html" + newLine;
+    headers += "Last-Modified: Tue, 06 Mar 2012 16:11:25 GMT" + newLine;
+    headers += "Content-Length: " + bodyLength + newLine;
+
+    std::string toSend = headers + newLine + body;
+    write(nextConnection, toSend.c_str(), toSend.length());
 
     socketApi.close(nextConnection);
   }
