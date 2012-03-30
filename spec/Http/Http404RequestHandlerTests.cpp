935a8188a68c1b0173c5ff6da25413addf09618a
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-30 14:36:13 -0500

    Refactoring.

--- spec/Http/Http404RequestHandlerTests.cpp
+++ spec/Http/Http404RequestHandlerTests.cpp
@@ -8,28 +8,30 @@ class Http404RequestHandlerTests
   : public ::testing::Test
 {
   public:
+    std::string getBody( const char* url )
+    {
+      HttpRequest request( url );
+      Http404RequestHandler handler;
 
+      HttpResponse* response = handler.handle( request );
+      std::string body = response->body();
+      
+      return body;
+    }
 };
 
 TEST_F( Http404RequestHandlerTests, bodyContainsUrlRequested )
 {
-  HttpRequest request( "invalid-url.html" );
-  Http404RequestHandler handler;
-
-  HttpResponse* response = handler.handle( request );
-  std::string body = response->body();
+  const char* url = "invalid-url.html";
+  std::string body = getBody( url );
   
-  ASSERT_LE( (size_t) 0 , body.find( "invalid-url.html" ) );
-  ASSERT_GT( body.length() , body.find( "invalid-url.html" ) );
+  ASSERT_LE( (size_t) 0 , body.find( url ) );
+  ASSERT_GT( body.length() , body.find( url ) );
 }
 
 TEST_F( Http404RequestHandlerTests, bodyContainsNotFoundLiteral )
 {
-  HttpRequest request( "" );
-  Http404RequestHandler handler;
-
-  HttpResponse* response = handler.handle( request );
-  std::string body = response->body();
+  std::string body = getBody( "" );
   
   ASSERT_LE( (size_t) 0 , body.find( "not found" ) );
   ASSERT_GT( body.length() , body.find( "not found" ) );
