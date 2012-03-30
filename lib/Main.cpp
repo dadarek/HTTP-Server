ecdc1deba4fae6c6924fd8297a1d0daa75ff5826
Dariusz Pasciak <dariusz@8thlight.com>
2012-03-30 16:59:48 -0500

    Updating main to use HttpRequesetHandlerFactoryImpl.

--- lib/Main.cpp
+++ lib/Main.cpp
@@ -1,46 +1,34 @@
-#include "Server.h"
-#include "SocketConnectionReceiver.h"
 #include "HttpConnectionHandler.h"
-#include "SocketReader.h"
-#include "HttpRequestHandlerFactory.h"
-#include "HttpResponseSocketWriter.h"
-#include "HttpRequestFileHandler.h"
-#include "SystemFileFactory.h"
-#include "SystemFileApi.h"
-#include "HttpSocketReader.h"
+#include "HttpRequestHandlerFactoryImpl.h"
 #include "HttpRequestParserImpl.h"
+#include "HttpResponseSocketWriter.h"
+#include "HttpSocketReader.h"
+
 #include "RawSocketApi.h"
 
-class DummyFactory
-  : public HttpRequestHandlerFactory
-{
-  public:
-    SystemFileFactory fileFactory_;
-    SystemFileApi fileApi_;
+#include "Server.h"
+#include "SocketConnectionReceiver.h"
+#include "SystemFileFactory.h"
+#include "SystemFileApi.h"
 
-    DummyFactory()
-      : fileFactory_()
-      , fileApi_( fileFactory_ )
-    { }
-    virtual ~DummyFactory()
-    { }
-
-    HttpRequestHandler* createHandler( HttpRequest& )
-    {
-      return new HttpRequestFileHandler("/Users/dariusz/Projects/HttpServer/public/", fileApi_ );
-    }
-};
+#include <string>
 
 int main()
 {
   RawSocketApi socketApi;
   HttpSocketReader socketReader( socketApi );
-  HttpRequestParserImpl requestParser;
-  DummyFactory factory;
   HttpResponseSocketWriter socketWriter( socketApi );
 
+  SystemFileFactory fileFactory;
+  SystemFileApi fileApi( fileFactory );
+  std::string basePath( "/Users/dariusz/Projects/HttpServer/public/" );
+
+  HttpRequestParserImpl requestParser;
+
+  HttpRequestHandlerFactoryImpl requestHandlerFactory( basePath, fileApi ); 
+
   HttpConnectionHandler connectionhandler
-    ( socketReader, requestParser, factory, socketWriter );
+    ( socketReader, requestParser, requestHandlerFactory, socketWriter );
 
   SocketConnectionReceiver receiver( socketApi, 5000 );
 
