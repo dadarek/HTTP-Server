af9c36ffd8b866b24b6a3b9b1fffcf7f2fe9022e
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-02 17:01:21 -0500

    Slow refactoring.

--- lib/Http/HttpRequestParserImpl.cpp
+++ lib/Http/HttpRequestParserImpl.cpp
@@ -10,17 +10,21 @@ HttpRequestParserImpl::~HttpRequestParserImpl()
 
 HttpRequest* HttpRequestParserImpl::parse( std::string headers )
 { 
-  int urlStartIndex = headers.find(" ") + 1;
-  int urlEndIndex = headers.find(" HTTP/1.1\r\n");
+  int firstSpaceIndex = headers.find(" ");
+  int httpVersionIndex = headers.find(" HTTP/1.1\r\n");
 
-  if ( urlStartIndex < 1 || urlEndIndex < urlStartIndex )
+  if ( firstSpaceIndex < 1 || httpVersionIndex < firstSpaceIndex )
     throw InvalidHttpRequestHeadersException();
+
+
+  int urlStartIndex = firstSpaceIndex + 1;
+  int urlEndIndex = httpVersionIndex;
 
   int urlLength = urlEndIndex - urlStartIndex;
 
   std::string url = headers.substr( urlStartIndex, urlLength );
-
   std::string method = headers.substr( 0, urlStartIndex-1 );
+
   return new HttpRequest( method, url );
 }
 
