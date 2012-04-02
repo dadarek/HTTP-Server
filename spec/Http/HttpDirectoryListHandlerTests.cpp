59e0b161da502b981d7af70328ce04d656673d73
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-02 14:54:11 -0500

    Refactoring.

--- spec/Http/HttpDirectoryListHandlerTests.cpp
+++ spec/Http/HttpDirectoryListHandlerTests.cpp
@@ -7,30 +7,33 @@
 class HttpDirectoryListRequestHandlerTests
   : public ::testing::Test
 {
+  public:
+    MockDirectoryApi directoryApi_;
+    std::string basePath_;
+    std::string url_;
+    HttpRequest request_;
+    HttpDirectoryListRequestHandler handler_;
+
+    HttpDirectoryListRequestHandlerTests()
+      : directoryApi_()
+      , basePath_( "/some/base/" )
+      , url_( "some/folder/ ")
+      , request_( url_ )
+      , handler_( basePath_, directoryApi_)
+    { }
+    
 
 };
 
 TEST_F( HttpDirectoryListRequestHandlerTests, OpensCorrectFolder )
 {
-  MockDirectoryApi directoryApi;
-  std::string basePath( "/some/base/" );
-  std::string url( "/some/folder/" );
-  HttpDirectoryListRequestHandler handler( basePath, directoryApi );
-
-  HttpRequest request( url );
-  handler.handle(request);
-
-  ASSERT_EQ( basePath + url, directoryApi.directoryOpened_ );
+  handler_.handle(request_);
+  ASSERT_EQ( basePath_ + url_, directoryApi_.directoryOpened_ );
 }
 
 TEST_F( HttpDirectoryListRequestHandlerTests, CallsReadDirWithCorrectDIRPointer )
 {
-  MockDirectoryApi directoryApi;
-  HttpDirectoryListRequestHandler handler( "", directoryApi );
-  HttpRequest request( "" );
-
-  directoryApi.opendir_returnValue_ = (DIR*) 77;
-  handler.handle( request );
-
-  ASSERT_EQ( (DIR*) 77, directoryApi.readdir_input_ );
+  directoryApi_.opendir_returnValue_ = (DIR*) 77;
+  handler_.handle( request_ );
+  ASSERT_EQ( (DIR*) 77, directoryApi_.readdir_input_ );
 }
