48d37b9f84290fa486493ab95db35aaa7a43489c
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-04 11:29:52 -0500

    Making sure mutex is destroyed in destructor.

--- lib/Threads/ThreadPool.cpp
+++ lib/Threads/ThreadPool.cpp
@@ -3,10 +3,11 @@
 ThreadPool::ThreadPool( ThreadApi& api )
   : api_( api )
 {
-  api_.pthread_mutex_destroy( &mutex_ );
   api_.pthread_mutex_init( &mutex_, 0 );
 }
 
 ThreadPool::~ThreadPool()
-{ }
+{ 
+  api_.pthread_mutex_destroy( &mutex_ );
+}
 
--- spec/Threads/ThreadPoolTests.cpp
+++ spec/Threads/ThreadPoolTests.cpp
@@ -10,11 +10,12 @@ TEST( ThreadPoolTests, InitsAValidMutex )
   ASSERT_NE( (void*) 0, api.mutexInit_mutex_input_ );
 }
 
-TEST( ThreadPoolTests, DestroysItsMutex )
+TEST( ThreadPoolTests, DestroysItsMutexInDestructor )
 {
   MockThreadApi api;
   {
     ThreadPool pool( api );
+    ASSERT_EQ( (void*) 0, api.mutexDestroy_mutex_input_ );
   }
 
   ASSERT_EQ( api.mutexInit_mutex_input_, api.mutexDestroy_mutex_input_ );
