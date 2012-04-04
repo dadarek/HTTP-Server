47e75c5edc0db10544bb416cc3c4897c3b54211b
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-04 10:43:04 -0500

    Playing around.

--- lib/Main.cpp
+++ lib/Main.cpp
@@ -18,8 +18,65 @@
 #include <string>
 #include <stdexcept>
 
+#include <pthread.h>
 
+pthread_mutex_t mutex;
+pthread_cond_t condition;
+
+void* threadFunction( void* )
+{
+  printf("Subthread locking mutex.\n");
+  pthread_mutex_lock( &mutex );
+
+  printf("Subthread waiting on signal.\n");
+  pthread_cond_wait( &condition, &mutex );
+
+  printf("Subthrea unlocking mutex.\n");
+  pthread_mutex_unlock( &mutex );
+
+  pthread_exit( 0 );
+}
 void go()
+{
+  /* Initialize mutex and condition variable objects */
+  pthread_mutex_init( &mutex, NULL );
+  pthread_cond_init( &condition, NULL );
+
+  long threadId;
+  pthread_t thread;
+
+  printf("About to create thread.\n");
+  pthread_create( &thread, 0, &threadFunction, (void*) &threadId );
+  printf("Thread created.\n");
+  printf("sleeping for 2.\n");
+  sleep( 2 );
+ 
+  printf("loking mutex.\n");
+  pthread_mutex_lock( &mutex );
+  printf("sleeping for 2.\n");
+  sleep( 2 );
+
+  printf("signalling condition.\n");
+  pthread_cond_signal( &condition );
+  printf("sleeping for 2.\n");
+  sleep( 2 );
+
+  printf("Unlocking mutex.");
+  pthread_mutex_unlock( &mutex );
+  printf("sleeping for 2.\n");
+  sleep( 2 );
+
+  printf("Joining thread.\n");
+  pthread_join( thread, 0 );
+  printf("Thread joined.\n");
+
+  pthread_mutex_destroy( &mutex );
+  pthread_cond_destroy( &condition );
+
+  pthread_exit( 0 );
+}
+
+void G()
 {
   SystemThreadApi threadApi;
 
