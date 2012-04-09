7d5a357aa5e2f79e2442e4ae844abbe5840281d8
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-09 09:42:37 -0500

    Adding ability to specify port and directory via command line.

--- lib/Main.cpp
+++ lib/Main.cpp
@@ -22,7 +22,7 @@
 
 #include <stdexcept>
 
-void go()
+void go( int portNumber, const char* directory )
 {
   SystemThreadApi threadApi;
 
@@ -34,7 +34,7 @@ void go()
 
   SystemFileFactory fileFactory;
   SystemFileApi fileApi( fileFactory );
-  std::string basePath( "/Users/dariusz/Projects/HttpServer/public/" );
+  std::string basePath( directory );
 
   HttpRequestParserImpl requestParser;
 
@@ -50,19 +50,46 @@ void go()
   ThreadedRequestHandler connectionhandler
     ( socketReader, requestParser, requestHandlerFactory, socketWriter, threadPool );
 
-  SocketConnectionReceiver receiver( socketApi, 5000 );
+  SocketConnectionReceiver receiver( socketApi, portNumber );
 
   Server server( receiver, connectionhandler );
 
   server.start();
 }
 
-
-int main()
+void ensureParameterCount( int argc )
 {
+  if( 3 != argc )
+  {
+    printf("Usage: ./main [port] [directory]\n\n");
+    exit( -1 );
+  }
+}
+
+void ensureDirectoryTrailingSlash( const char* directory )
+{
+  int directoryLength = strlen( directory );
+  char lastCharacter = directory[ directoryLength - 1 ];
+  if( '/' !=  lastCharacter )
+  {
+    printf("Help us protect the environment - please enter a trailing backslash on your directory.\n\n");
+    exit( -1 );
+  }
+}
+
+int main( int argc, char** argv )
+{
+
+  ensureParameterCount( argc );
+  ensureDirectoryTrailingSlash( argv[2] );
+
+  int port = atoi( argv[1] );
+  const char* directory = argv[2];
+  
+
   try
   {
-    go();
+    go( port, directory );
   }
   catch( std::runtime_error e )
   {
