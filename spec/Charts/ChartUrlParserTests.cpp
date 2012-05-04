9e05622a8a7176c694a6777b65898a0946f7919e
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-04 17:37:13 -0500

    Refactoring.

--- spec/Charts/ChartUrlParserTests.cpp
+++ spec/Charts/ChartUrlParserTests.cpp
@@ -5,28 +5,31 @@ class ChartUrlParserTests
   : public ::testing::Test
 {
   public:
+    static std::string OPEN_BRACE;
+    static std::string CLOSE_BRACE;
+    static std::string QUOTE;
     ChartUrlParser parser;
 };
 
+std::string ChartUrlParserTests::OPEN_BRACE = "%7B";
+std::string ChartUrlParserTests::CLOSE_BRACE = "%7D";
+std::string ChartUrlParserTests::QUOTE= "%22";
+
 TEST_F( ChartUrlParserTests, parsesOneLog )
 {
-  std::string opening_brace = "%7B";
-  std::string closing_brace = "%7D";
-  std::string quote = "%22";
-
   std::string date_ran = "2012-04-15";
   std::string time_ran = "30";
 
   std::string json = 
     "[" + 
-    opening_brace + 
-    quote + "date_ran" + quote + 
+    OPEN_BRACE + 
+    QUOTE + "date_ran" + QUOTE + 
     ":" + 
-    quote + date_ran + quote +
-    quote + "time_ran" + quote + 
+    QUOTE + date_ran + QUOTE +
+    QUOTE + "time_ran" + QUOTE + 
     ":" + 
-    quote + time_ran + quote +
-    closing_brace +
+    QUOTE + time_ran + QUOTE +
+    CLOSE_BRACE +
     "]";
   ASSERT_EQ(date_ran, parser.parseDate(json));
   ASSERT_EQ(time_ran, parser.parseTime(json));
@@ -34,15 +37,15 @@ TEST_F( ChartUrlParserTests, parsesOneLog )
 
 TEST_F( ChartUrlParserTests, DecodesSingleCharacters )
 {
-  ASSERT_EQ("{", parser.urlDecode("%7B"));
-  ASSERT_EQ("}", parser.urlDecode("%7D"));
-  ASSERT_EQ("\"", parser.urlDecode("%22"));
+  ASSERT_EQ("{", parser.urlDecode(OPEN_BRACE));
+  ASSERT_EQ("}", parser.urlDecode(CLOSE_BRACE));
+  ASSERT_EQ("\"", parser.urlDecode(QUOTE));
 }
 
 TEST_F( ChartUrlParserTests, DecodesMultipleCharacters )
 {
-  std::string code("%7B%7D%7B%7B%22");
-  ASSERT_EQ("{}{{\"", parser.urlDecode(code));
+  std::string code = OPEN_BRACE + CLOSE_BRACE + OPEN_BRACE + QUOTE;
+  ASSERT_EQ("{}{\"", parser.urlDecode(code));
 }
 
 TEST_F( ChartUrlParserTests, DecodesMixedCodes )
