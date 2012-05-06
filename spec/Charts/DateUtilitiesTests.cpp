d307f60f6a448208a4c2067346772b8d49df565e
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-05 19:49:48 -0500

    Simple test of subtraction.

--- spec/Charts/DateUtilitiesTests.cpp
+++ spec/Charts/DateUtilitiesTests.cpp
@@ -14,3 +14,20 @@ TEST( DateUtilitiesTests, SetsDayOfWeekCorrectly )
 
   ASSERT_EQ( 6, expected.tm_wday );
 }
+
+TEST( DateUtilitiesTests, SubtractsDaysCorrectly )
+{
+  struct tm expected;
+  memset( &expected, 0, sizeof(expected) );
+
+  expected.tm_year = 2012 - 1900;
+  expected.tm_mon = 5 - 1;
+  expected.tm_mday = 5 - 15;
+
+  mktime( &expected );
+
+  ASSERT_EQ( 5, expected.tm_wday );
+  ASSERT_EQ( 20, expected.tm_mday );
+  ASSERT_EQ( 4-1, expected.tm_mon );
+  ASSERT_EQ( 2012-1900, expected.tm_year );
+}
