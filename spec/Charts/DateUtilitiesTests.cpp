e4b3c21eb025f1c32012e9a0b1b64b9fe4e83215
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-05 19:57:31 -0500

    Comparison tests.

--- spec/Charts/DateUtilitiesTests.cpp
+++ spec/Charts/DateUtilitiesTests.cpp
@@ -31,3 +31,26 @@ TEST( DateUtilitiesTests, SubtractsDaysCorrectly )
   ASSERT_EQ( 4-1, expected.tm_mon );
   ASSERT_EQ( 2012-1900, expected.tm_year );
 }
+
+TEST( DateUtilitiesTests, ComparesCorrectly )
+{
+  struct tm may_5;
+  memset( &may_5, 0, sizeof(may_5) );
+  may_5.tm_year = 2012 - 1900;
+  may_5.tm_mon = 5 - 1;
+  may_5.tm_mday = 5;
+  time_t t_may_5 = mktime( &may_5 );
+
+  struct tm may_6;
+  memset( &may_6, 0, sizeof(may_6) );
+  may_6.tm_year = 2012 - 1900;
+  may_6.tm_mon = 5 - 1;
+  may_6.tm_mday = 6;
+  time_t t_may_6 = mktime( &may_6 );
+
+  ASSERT_GT( (double) 0, difftime(t_may_5, t_may_6) );
+  ASSERT_LT( (double) 0, difftime(t_may_6, t_may_5) );
+  ASSERT_EQ( (double) 0, difftime(t_may_5, t_may_5) );
+  ASSERT_EQ( (double) 0, difftime(t_may_6, t_may_6) );
+}
+
