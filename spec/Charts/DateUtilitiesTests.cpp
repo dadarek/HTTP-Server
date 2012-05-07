c4935f14f32616a18d6f51af5a9f2b68bc053bdb
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-06 22:52:36 -0500

    Refactoring.

--- spec/Charts/DateUtilitiesTests.cpp
+++ spec/Charts/DateUtilitiesTests.cpp
@@ -1,30 +1,36 @@
 #include "gtest/gtest.h"
 #include "DateUtilities.h"
 
-TEST( DateUtilitiesTests, SetsDayOfWeekCorrectly )
+class DateUtilitiesTests
+  : public ::testing::Test
+{
+  public:
+    DateUtilitiesTests(){ }
+
+    time_t createDate(struct tm& date, int year, int month, int day)
+    {
+      memset( &date, 0, sizeof(date) );
+
+      date.tm_year = year - 1900;
+      date.tm_mon = month - 1;
+      date.tm_mday = day;
+
+      time_t result = mktime( &date );
+      return result;
+    }
+};
+
+TEST_F( DateUtilitiesTests, SetsDayOfWeekCorrectly )
 {
   struct tm expected;
-  memset( &expected, 0, sizeof(expected) );
-
-  expected.tm_year = 2012 - 1900;
-  expected.tm_mon = 5 - 1;
-  expected.tm_mday = 5;
-
-  mktime( &expected );
-
-  ASSERT_EQ( 6, expected.tm_wday );
+  createDate(expected, 2012, 5, 4);
+  ASSERT_EQ( 5, expected.tm_wday );
 }
 
-TEST( DateUtilitiesTests, SubtractsDaysCorrectly )
+TEST_F( DateUtilitiesTests, SubtractsDaysCorrectly )
 {
   struct tm expected;
-  memset( &expected, 0, sizeof(expected) );
-
-  expected.tm_year = 2012 - 1900;
-  expected.tm_mon = 5 - 1;
-  expected.tm_mday = 5 - 15;
-
-  mktime( &expected );
+  createDate(expected, 2012, 5, 5-15);
 
   ASSERT_EQ( 5, expected.tm_wday );
   ASSERT_EQ( 20, expected.tm_mday );
@@ -32,21 +38,11 @@ TEST( DateUtilitiesTests, SubtractsDaysCorrectly )
   ASSERT_EQ( 2012-1900, expected.tm_year );
 }
 
-TEST( DateUtilitiesTests, ComparesCorrectly )
+TEST_F( DateUtilitiesTests, ComparesCorrectly )
 {
-  struct tm may_5;
-  memset( &may_5, 0, sizeof(may_5) );
-  may_5.tm_year = 2012 - 1900;
-  may_5.tm_mon = 5 - 1;
-  may_5.tm_mday = 5;
-  time_t t_may_5 = mktime( &may_5 );
-
-  struct tm may_6;
-  memset( &may_6, 0, sizeof(may_6) );
-  may_6.tm_year = 2012 - 1900;
-  may_6.tm_mon = 5 - 1;
-  may_6.tm_mday = 6;
-  time_t t_may_6 = mktime( &may_6 );
+  struct tm date;
+  time_t t_may_5 = createDate(date, 2012, 5, 5);
+  time_t t_may_6 = createDate(date, 2012, 5, 6);
 
   ASSERT_GT( (double) 0, difftime(t_may_5, t_may_6) );
   ASSERT_LT( (double) 0, difftime(t_may_6, t_may_5) );
